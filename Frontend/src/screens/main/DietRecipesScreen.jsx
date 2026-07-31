034fb7f89533c93b770b3dece15f47bb0866e1b1
Bob Kaizer Alap-ap <bobkaizer6@gmail.com>
2026-07-31 17:20:50 +0800

    Add missing WebView import to DietRecipesScreen.jsx

--- Frontend/src/screens/main/DietRecipesScreen.jsx
+++ Frontend/src/screens/main/DietRecipesScreen.jsx
@@ -21,6 +21,7 @@ import { addToSyncQueue, updateCachedDashboardField } from '../../services/Offli
 import { useCustomAlert } from '../../context/CustomAlertContext';
 import { useTheme } from '../../context/ThemeContext';
 import AILoadingModal from '../../components/AILoadingModal';
+import { WebView } from 'react-native-webview';
 const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
 
 const pushNotificationIfAllowed = async (newNotif, setNotifications) => {
