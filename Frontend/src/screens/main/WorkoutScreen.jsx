eda7905f866ac493ab617fd3499465e52294ee20
Bob Kaizer Alap-ap <bobkaizer6@gmail.com>
2026-08-27 01:26:17 +0800

    show full workout description

--- Frontend/src/screens/main/WorkoutScreen.jsx
+++ Frontend/src/screens/main/WorkoutScreen.jsx
@@ -577,7 +577,7 @@ export default function WorkoutScreen({
               <View style={styles.workoutHeaderRow}>
                 <View style={styles.workoutTitleContainer}>
                   <Text style={styles.workoutMainTitle}>{workout.title}</Text>
-                  <Text style={styles.workoutDescriptionText} numberOfLines={2} ellipsizeMode="tail">{workout.description}</Text>
+                  <Text style={styles.workoutDescriptionText}>{workout.description}</Text>
                 </View>
               </View>
 
