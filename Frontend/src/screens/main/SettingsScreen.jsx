50acd5986977b064bc8a308b00233b2ec66919dd
Necolie <necoliejamescanales@gmail.com>
2026-09-05 10:42:32 +0800

    Remove leftover git conflict marker in SettingsScreen.jsx

--- Frontend/src/screens/main/SettingsScreen.jsx
+++ Frontend/src/screens/main/SettingsScreen.jsx
@@ -1144,7 +1144,6 @@ export default function SettingsScreen({
           </View>
         </View>
 
->>>>>>> c5998a5302c2ed4e2540d30d09124ccc3a54132b
         {/* NOTIFICATIONS SETTINGS CARD */}
         <Text style={styles.sectionLabelTitle}>Notification Settings</Text>
         <View style={styles.formCard}>
@@ -1830,7 +1829,7 @@ export default function SettingsScreen({
               </Text>
             </TouchableOpacity>
 
-            {userProfile?.profileImage && !imageError && (
+            {!!userProfile?.profileImage && !imageError && (
               <TouchableOpacity
                 onPress={() => {
                   setShowPhotoPreviewModal(false);
